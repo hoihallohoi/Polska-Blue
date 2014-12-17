f55d00beb7f34a7bb5afab1d1a0d22ed6686fbfe
richi-ruler <richardkooijker@hotmail.com>
2014-12-17 14:57:12 +0100

    jajaja
    jajaja weer een codee rbij..!

--- info_ophalen.php
+++ info_ophalen.php
@@ -33,22 +33,25 @@ and open the template in the editor.
                 </ul>
             </form>
         </div>
-        <div class="mid"><p>Welkom bij Polska Blue.</p> <br>
-            Polska Blue heeft een prachtige collectie  Bunzlau servies, Pools aardewerk op voorraad. <br>Helaas is de webshop nog niet gevuld, hier wordt aan gewerkt.   <br>
-            Kom gezellig eens binnen lopen zonder een afspraak te maken.<br>Andere dagen? Neem dan even contact op.<br>
-                                          <br>
-                                          Open op woensdag en donderdagmiddag<br>
-                                          van 13.30 tot 18.00 uur.<br>
-<br>
-                                          Adresgegevens:<br> Anita de Vries<br>
-                                                                       Eemnesserweg 58<br>
-                                                                        3741GB Baarn<br>
-                                                             mobiel :  06 216 55555    <br> 
-                                                             mailadres : polskablue@gmail.com<br>
- <br>
-                                          Handig om te weten!<br>
-                                          Komt u een bezoekje brengen in de showroom ontvangt u extra 10 % korting op de toch al lagere prijzen...       <br>                             
-                                          ook kunt u het servies laten opsturen. Vanaf 50 euro zijn de verzendkosten voor mijn rekening.<br>
+        <div class="mid">
+            <?php
+            
+            //Connecting met sql db
+                $conn = mysqli_connect("localhost", "root", "usbw", "info_toevoegen", 3307);
+            
+            //hier haal je de GET op die meegegeven is
+            $links=$_GET["links"];
+            //prepared statement voor data ophalen      
+            $stmt = mysqli_prepare($conn, "SELECT informatie FROM informatie_polskablue WHERE links=?");        
+            mysqli_stmt_bind_param($stmt, "s", $links);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $info);
+            mysqli_stmt_fetch($stmt);
+            print ($info);
+            
+            mysqli_close($conn)
+
+            ?>
         </div>
         <img class="right" src="Foto/IMG_8964.JPG">
         <div class="left"><br><h2>Producten</h2>
